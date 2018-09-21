b5023e78cab5adb6a32a8ace4b933970ef4e167e
Elena Granados <patty518@yahoo.com>
2018-09-21 15:43:11 -0700

    modified readme and server to fit my app

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
-## "Shakespeare Passport" | Thinkful Node Capstone
-The Shakespeare Passport app is designed for Theatrical Performers, Theatre Goers, and Theatre enthusiast alike. It allows a User to compile a list of plays by SEEN, PERFORMED, and READ tracking date and location.
+## "Epic Journey" | Thinkful Node Capstone
+Epic Journey app helps users to keep track of the places they visit by adding location, a description or journal entry, and photos or videos of their trip.   Each trip can be broken down into smaller trips, for instance, going to Greece can be broken into Athens, Mikonos, Delphi, etc., with each smaller trip having its own location, journal entry, and photos.  
 
 ## Screenshots
 
@@ -12,12 +12,6 @@ Add Entry  | Edit Entry
 ![Add Entry](https://github.com/KatiLong/shakespeare-passport-node-capstone/blob/master/github-images/add-entry.jpg) | ![Edit Entry](https://github.com/KatiLong/shakespeare-passport-node-capstone/blob/master/github-images/edit-entry.jpg)
 Delete Entry |
 ![Delete Entry](https://github.com/KatiLong/shakespeare-passport-node-capstone/blob/master/github-images/delete-entry.jpg) |
-
-## User Cases
-This app is for three types of users:
-1. READ - For Theatrical Students, so that they can track Plays that have been read (by title and author, optional edition) by date and include Notes.
-2. SEEN - For Theatre-Goers (enthusiasts, specialists, etc.), so that they can track all the shows the have seen, where and when they have seen them, and any specific Notes they would like to add.
-3. PERFORMED - For Theatrical Artists and Perfomers, so that they can record all shows they have performed in by Role, Location, Date and include Notes (and Lines).
 
 ### UI Flow
 ![UI Flow handwritten draft](https://github.com/KatiLong/node-capstone/blob/master/github-images/node-capstone-user-flow.jpg)
@@ -32,7 +26,6 @@ You can access a working prototype of the app here: https://epic-journey.herokua
 The app's functionality includes:
 * Every User has the ability to create an account that stores information unique to them
 * User can Add Entries, Update Entries, and Delete Entries
-* User can sort entries by: Date & Type (Read, Seen, Performed)
 
 ## Technology
 * Front-End: HTML5 | CSS3 | JavaScript ES6 | jQuery
@@ -45,5 +38,7 @@ App is strongly built to be usuable on mobile devices, as well as responsive acr
 
 ## Development Roadmap
 This is v1.0 of the app, but future enhancements are expected to include:
-* Google Maps integrated to view all entries by location
-* Featured Theaters of the World Section
+* Ability to share journeys with friends
+* Ability to have friends post, edit, and delete comments on journeys
+
+
--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.use(express.static('public'));
 
 mongoose.Promise = global.Promise;
 
+
+
+
+
+
 // ---------------- RUN/CLOSE SERVER -----------------------------------------------------
 let server = undefined;
 
@@ -117,7 +122,7 @@ app.post('/users/create', (req, res) => {
 });
 
 // signing in a user
-app.post('/users/login', function (req, res) {
+app.post('/users/login', function(req, res) {
 
     //take the username and the password from the ajax api call
     const username = req.body.username;
@@ -126,7 +131,7 @@ app.post('/users/login', function (req, res) {
     //using the mongoose DB schema, connect to the database and the user with the same username as above
     User.findOne({
         username: username
-    }, function (err, items) {
+    }, function(err, items) {
 
         //if the there is an error connecting to the DB
         if (err) {
@@ -147,7 +152,7 @@ app.post('/users/login', function (req, res) {
         else {
 
             //try to validate the password
-            items.validatePassword(password, function (err, isValid) {
+            items.validatePassword(password, function(err, isValid) {
 
                 //if the connection to the DB to validate the password is not working
                 if (err) {
@@ -213,22 +218,22 @@ app.post('/entry/create', (req, res) => {
 });
 
 // PUT --------------------------------------
-app.put('/entry/:id', function (req, res) {
+app.put('/entry/:id', function(req, res) {
     let toUpdate = {};
     //    let updateableFields = ['achieveWhat', 'achieveHow', 'achieveWhen', 'achieveWhy']; //<--Marius? 'entryType
     let updateableFields = ['entryType', 'inputDate', 'inputPlay', 'inputAuthor', 'inputRole', 'inputCo', 'inputLocation', 'inputNotes', 'loggedInUserName'];
-    updateableFields.forEach(function (field) {
+    updateableFields.forEach(function(field) {
         if (field in req.body) {
             toUpdate[field] = req.body[field];
         }
     });
-//    console.log(toUpdate);
+    //    console.log(toUpdate);
     Entry
         .findByIdAndUpdate(req.params.id, {
             $set: toUpdate
-        }).exec().then(function (achievement) {
+        }).exec().then(function(achievement) {
             return res.status(204).end();
-        }).catch(function (err) {
+        }).catch(function(err) {
             return res.status(500).json({
                 message: 'Internal Server Error'
             });
@@ -237,14 +242,14 @@ app.put('/entry/:id', function (req, res) {
 
 // GET ------------------------------------
 // accessing all of a user's entries
-app.get('/entry-date/:user', function (req, res) {
+app.get('/entry-date/:user', function(req, res) {
 
     Entry
         .find()
         .sort('inputDate')
-        .then(function (entries) {
+        .then(function(entries) {
             let entriesOutput = [];
-            entries.map(function (entry) {
+            entries.map(function(entry) {
                 if (entry.loggedInUserName == req.params.user) {
                     entriesOutput.push(entry);
                 }
@@ -253,23 +258,23 @@ app.get('/entry-date/:user', function (req, res) {
                 entriesOutput
             });
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.error(err);
             res.status(500).json({
                 message: 'Internal server error'
             });
         });
 });
-app.get('/entry-read/:user', function (req, res) {
+app.get('/entry-read/:user', function(req, res) {
 
     Entry
         .find({
             "entryType": "read"
         })
         .sort('inputDate')
-        .then(function (entries) {
+        .then(function(entries) {
             let entriesOutput = [];
-            entries.map(function (entry) {
+            entries.map(function(entry) {
                 if (entry.loggedInUserName == req.params.user) {
                     entriesOutput.push(entry);
                 }
@@ -278,23 +283,23 @@ app.get('/entry-read/:user', function (req, res) {
                 entriesOutput
             });
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.error(err);
             res.status(500).json({
                 message: 'Internal server error'
             });
         });
 });
-app.get('/entry-seen/:user', function (req, res) {
+app.get('/entry-seen/:user', function(req, res) {
 
     Entry
         .find({
             "entryType": "seen"
         })
         .sort('inputDate')
-        .then(function (entries) {
+        .then(function(entries) {
             let entriesOutput = [];
-            entries.map(function (entry) {
+            entries.map(function(entry) {
                 if (entry.loggedInUserName == req.params.user) {
                     entriesOutput.push(entry);
                 }
@@ -303,23 +308,23 @@ app.get('/entry-seen/:user', function (req, res) {
                 entriesOutput
             });
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.error(err);
             res.status(500).json({
                 message: 'Internal server error'
             });
         });
 });
-app.get('/entry-performed/:user', function (req, res) {
+app.get('/entry-performed/:user', function(req, res) {
 
     Entry
         .find({
             "entryType": "performed"
         })
         .sort('inputDate')
-        .then(function (entries) {
+        .then(function(entries) {
             let entriesOutput = [];
-            entries.map(function (entry) {
+            entries.map(function(entry) {
                 if (entry.loggedInUserName == req.params.user) {
                     entriesOutput.push(entry);
                 }
@@ -328,7 +333,7 @@ app.get('/entry-performed/:user', function (req, res) {
                 entriesOutput
             });
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.error(err);
             res.status(500).json({
                 message: 'Internal server error'
@@ -337,12 +342,12 @@ app.get('/entry-performed/:user', function (req, res) {
 });
 
 // accessing a single achievement by id
-app.get('/entry/:id', function (req, res) {
+app.get('/entry/:id', function(req, res) {
     Entry
-        .findById(req.params.id).exec().then(function (entry) {
+        .findById(req.params.id).exec().then(function(entry) {
             return res.json(entry);
         })
-        .catch(function (entries) {
+        .catch(function(entries) {
             console.error(err);
             res.status(500).json({
                 message: 'Internal Server Error'
@@ -352,10 +357,10 @@ app.get('/entry/:id', function (req, res) {
 
 // DELETE ----------------------------------------
 // deleting an achievement by id
-app.delete('/entry/:id', function (req, res) {
-    Entry.findByIdAndRemove(req.params.id).exec().then(function (entry) {
+app.delete('/entry/:id', function(req, res) {
+    Entry.findByIdAndRemove(req.params.id).exec().then(function(entry) {
         return res.status(204).end();
-    }).catch(function (err) {
+    }).catch(function(err) {
         return res.status(500).json({
             message: 'Internal Server Error'
         });
