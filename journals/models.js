ec60d199bcb62b0382beb60feff9c2e97c2f1935
Elena Granados <patty518@yahoo.com>
2018-09-27 13:29:28 -0700

    create journal API response

--- journals/models.js
+++ journals/models.js
@@ -9,5 +9,21 @@ const journeySchema = mongoose.Schema({
     location: { type: String, required: true },
     // dates: { type: },
     description: 'string',
-    created: { type: Date, default: Date.now }
+    created: { type: Date, default: Date.now },
+    // photos: {type: 'image'}
 });
+
+journeySchema.methods.serialize = function() {
+    return {
+        id: this._id,
+        title: this.title,
+        location: this.location,
+        dates: this.dates,
+        description: this.description,
+
+    }
+}
+
+const Journey = mongoose.model('Journey', journeySchema);
+
+module.exports = { Journey };
