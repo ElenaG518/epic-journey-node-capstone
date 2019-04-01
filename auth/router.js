d8e4b2249e680ce0ed1e2b71058a6e2589ed6704
Elena Granados <patty518@yahoo.com>
2019-03-31 17:37:11 -0700

    add strategies to auth router

--- auth/router.js
+++ auth/router.js
@@ -7,9 +7,33 @@ const jwt = require('jsonwebtoken');
 const config = require('../config');
 const router = express.Router();
 
-const localAuth = passport.authenticate('local', { session: false });
+const createAuthToken = function(user) {
+  // jwt.sign(payload, secretOrPrivateKey, [options, callback])
+  return jwt.sign({user}, config.JWT_SECRET, {
+    subject: user.username,
+    expiresIn: config.JWT_EXPIRY,
+    algorithm: 'HS256'
+  });
+};
+
+const localAuth = passport.authenticate('local', {session: false});
 router.use(bodyParser.json());
+// The user provides a username and password to login
+// The strategy retrieves the username and password from the req.body
+//  and passes them to a callback function, in this case localStategy in strategies.js
+router.post('/login', localAuth, (req, res) => {
+  // the user object is added to the request object at req.user
+  console.log("req.user from login in auth ", req.user);
+  const authToken = createAuthToken(req.user.serialize());
+  res.json({authToken});
+});
 
-const jwtAuth = passport.authenticate('jwt', { session: false });
+const jwtAuth = passport.authenticate('jwt', {session: false});
 
-module.exports = router;
+// The user exchanges a valid JWT for a new one with a later expiration
+router.post('/refresh', jwtAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.json({authToken});
+});
+
+module.exports = {router};
--- auth/strategies.js
+++ auth/strategies.js
@@ -8,48 +8,64 @@ const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const { User } = require('../users/models');
 const { JWT_SECRET } = require('../config');
 
+
+// allowed the user to supply a username and password to authenticate with an endpoint;
 const localStrategy = new LocalStrategy((username, password, callback) => {
-    let user;
-    User.findOne({ username: username })
-        .then(_user => {
-            user = _user;
-            if (!user) {
-                // Return a rejected promise so we break out of the chain of .thens.
-                // Any errors like this will be handled in the catch block.
-                return Promise.reject({
-                    reason: 'LoginError',
-                    message: 'Incorrect username or password'
-                });
-            }
-            return user.validatePassword(password);
-        })
-        .then(isValid => {
-            if (!isValid) {
-                return Promise.reject({
-                    reason: 'LoginError',
-                    message: 'Incorrect username or password'
-                });
-            }
-            return callback(null, user);
-        })
-        .catch(err => {
-            if (err.reason === 'LoginError') {
-                return callback(null, false, err);
-            }
-            return callback(err, false);
+  // we make a global variable within this app taht we can reference throughout the thenables
+  let user;
+  User.findOne({ username: username })
+    .then(_user => {
+      console.log("user ", _user);
+      user = _user;
+      if (!user) {
+        // Return a rejected promise so we break out of the chain of .thens.
+        // Any errors like this will be handled in the catch block.
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username or password'
         });
+      }
+      // returns a boolean value indicating whether or not the password is valid
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username or password'
+        });
+      }
+      // this is where we need to return user after the password has been verified as valid
+      // the user object will be added to the request object at req.user
+      return callback(null, user);
+    })
+    .catch(err => {
+      if (err.reason === 'LoginError') {
+        return callback(null, false, err);
+      }
+      return callback(err, false);
+    });
 });
 
-const jwtStrategy = new JwtStrategy({
-        secretOrKey: JWT_SECRET,
-        // Look for the JWT as a Bearer auth header
-        jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
-        // Only allow HS256 tokens - the same as the ones we issue
-        algorithms: ['HS256']
-    },
-    (payload, done) => {
-        done(null, payload.user);
-    }
+// new JwtStrategy(options, verify)
+// http://www.passportjs.org/packages/passport-jwt/
+
+// verify is a function with the parameters verify(jwt_payload, done)
+// jwt_payload is an object literal containing the decoded JWT payload.
+// done is a passport error first callback accepting arguments done(error, user, info)
+
+const jwtStrategy = new JwtStrategy(
+  {
+    secretOrKey: JWT_SECRET,
+    // Look for the JWT as a Bearer auth header
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+    // Only allow HS256 tokens - the same as the ones we issue
+    algorithms: ['HS256']
+  },
+  // payload is an object literal containing the decoded JWT payload.
+  (payload, done) => {
+    done(null, payload.user);
+  }
 );
 
 module.exports = { localStrategy, jwtStrategy };
