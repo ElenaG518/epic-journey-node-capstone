da095149a046f7ea0e6d855fe98bad99b66d62ba
Elena Granados <patty518@yahoo.com>
2019-03-31 17:49:18 -0700

    update models and router to use local stategy

--- users/models.js
+++ users/models.js
@@ -1,43 +1,39 @@
 'use strict';
-// cryptjs for hashing and validation passwords
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// DEFINE USER SCHEMA
 const UserSchema = mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    firstName: { type: String, default: '' },
-    lastName: { type: String, default: '' }
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {type: String, default: ''},
+  lastName: {type: String, default: ''}
 });
 
-// RETURN USER ITEM BUT DO NOT INCLUDE PASSWORD
 UserSchema.methods.serialize = function() {
-    return {
-        id: this._id,
-        username: this.username || '',
-        firstName: this.firstName || '',
-        lastName: this.lastName || ''
-    };
+  return {
+    username: this.username || '',
+    firstName: this.firstName || '',
+    lastName: this.lastName || ''
+  };
 };
 
 UserSchema.methods.validatePassword = function(password) {
-    return bcrypt.compare(password, this.password);
+  return bcrypt.compare(password, this.password);
 };
 
 UserSchema.statics.hashPassword = function(password) {
-    return bcrypt.hash(password, 10);
+  return bcrypt.hash(password, 10);
 };
 
 const User = mongoose.model('User', UserSchema);
 
-module.exports = { User };
+module.exports = {User};
